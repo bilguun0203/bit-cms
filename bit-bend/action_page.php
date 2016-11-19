fa33a82ba6653b38a3874d4e20bae886af68092a
Bilguun Ochirbat <bilguun0203@gmai.com>
2016-11-20 01:21:25 +0800

    Хуудас болон сэтгэгдлийн хэсгүүдийг функцын хамт нэмсэн

---
+++ bit-bend/action_page.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bilguun
+ * Date: 11/19/16
+ * Time: 11:56 PM
+ */
---
+++ views/backend/template/standard/edit_user.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bilguun
+ * Date: 11/20/16
+ * Time: 12:57 AM
+ */
